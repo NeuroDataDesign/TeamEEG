18e4fcc8b9f7b34649835fd479d011fa0f6ab9cb
rbechto2 <54995036+rbechto2@users.noreply.github.com>
2019-10-03 10:40:39 -0400

    Update preProcessRPCA.py

--- Raph/preProcessRPCA.py
+++ Raph/preProcessRPCA.py
@@ -1,25 +1,38 @@
 import numpy as np
 from sklearn.decomposition import TruncatedSVD
-
+"""
+    Cian Scannell - Oct-2017 (https://github.com/cianmscannell/RPCA)
+    Computes rpca separation of M into L(low rank) and S(Sparse) using the parameter lam
+    this uses the alternating directions augmented method of multipliers
+    as described in my blog 
+"""
 def rpca(M,lam,tol,maxIter):
 
-    # Cian Scannell - Oct-2017  
-    # computes rpca separation of M into L and S using the parameter lam
-
-    # this uses the alternating directions augmented method of multipliers
-    # as described in my blog 
-
+    """ Perform Robust Principle Component Analysis:
+    -Input = [M,lam,tol,maxIter]
+           EEG = EEG Data (must include)
+           lam = Lamda paramter for RPCA (default = 1/(sqrt(# of Colunms))
+           tol = Tolerance (defalut = 1e-7) RPCA param
+           maxIter = Maximum Iterations (deafult = 1000)
+    
+    -Output = [L, S]
+            L = Corrected Data (Low rank matrix)
+            S = Noise removed from the data (Sparse Matrix)
+            M = L + S        
+    
+    -Summary = Performs a Robust Principal Component Analysis on the EEG data with
+            the specified parameters: Lamda, Tolerance, and Maximum number of Iterations.
+            The function outputs the EEG data with the noise removed as well as the nosie
+            that was removed.
+"""
 
     Nr = M.shape[0]
     Nc = M.shape[1]
-
-
+    
     Y = M / np.maximum(np.linalg.norm(M,2), np.linalg.norm(M,np.inf) / lam)
-
 
     mu = 1/ (np.linalg.norm(M,2))
     rho = 1.5
-
 
     S = np.zeros((Nr,Nc))    
 
@@ -37,23 +50,17 @@ def rpca(M,lam,tol,maxIter):
         if (count >= maxIter):
             isRunning = False;
         
-
     L = L.reshape(Nr,Nc)
     S = S.reshape(Nr,Nc)
-
-    # used to previously return count also
-    # but don't really use this
 
     return L,S
 
 def soft_thres(x,eps):
-
-    # Cian Scannell - Oct-2017  
-
-    # soft thresholds a matrix x at the eps level
-    # i.e ST(x,eps)_ij = sgn(x_ij) max(|x_ij| - eps, 0)
-
+"""
+    Cian Scannell - Oct-2017  
+    Soft thresholds a matrix x at the eps level
+    i.e ST(x,eps)_ij = sgn(x_ij) max(|x_ij| - eps, 0)
+"""
     a = np.sign(x)
     b = np.maximum((np.fabs(x) - eps), 0)
     return np.multiply(a,b)
-
