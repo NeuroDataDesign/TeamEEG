7164be001ca1527c0b3e72d46562a6dacc0bcbc7
Raphael Bechtold <raphaelbechtold@Raphaels-MacBook-Pro-2.local>
2019-10-03 16:07:28 -0400

    made some tweaks to performRPCA and the preProcessRPCA functions

--- Raph/preProcessRPCA.py
+++ Raph/preProcessRPCA.py
@@ -30,27 +30,34 @@ def rpca(M,lam,tol,maxIter):
 
     Nr = M.shape[0]
     Nc = M.shape[1]
-    Y = M / np.maximum(np.linalg.norm(M,2), np.linalg.norm(M,np.inf) / lam)
+    norm_2 = np.linalg.norm(M,2)
+    norm_inf = np.linalg.norm(M,np.inf) / lam
+    dual_norm = np.maximum(norm_2, norm_inf)
+    Y = M / dual_norm
 
-    mu = 1/ (np.linalg.norm(M,2))
+    mu = 1.25 / norm_2
+    mu_bar = mu * 1e7
     rho = 1.5
 
+    L = np.zeros((Nr,Nc))
     S = np.zeros((Nr,Nc))    
     
     error = 10
     count = 0
     isRunning = True;
     while (isRunning and error > tol):
-        U,sig,V = np.linalg.svd(M-S+Y/mu, full_matrices=False)
+        temp_t = M-L+(Y/mu)
+        S = soft_thres(temp_t, lam/mu)
+        U,sig,V = np.linalg.svd(M-S+(Y/mu), full_matrices=False)
         L = np.dot(U, np.dot(np.diag(soft_thres(sig, 1/mu)), V))
-        S = soft_thres(M-L+Y/mu, lam/mu)
         Y = Y + mu*(M-L-S)
-        mu = mu*rho
-        error = np.linalg.norm(M-L-S,'fro') / np.linalg.norm(M,'fro')
+        mu = np.minimum(mu*rho,mu_bar)
+        error = np.linalg.norm(M-L-S,'fro')/np.linalg.norm(M,'fro')
         count += 1 
         if (count >= maxIter):
             isRunning = False;
-        
+            
+            
     L = L.reshape(Nr,Nc)
     S = S.reshape(Nr,Nc)
 
