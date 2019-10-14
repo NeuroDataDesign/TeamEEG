9f43763b8a7fd34becec7e2650fa151731fed31b
rbechto2 <54995036+rbechto2@users.noreply.github.com>
2019-10-14 11:53:17 -0400

    Update performRPCA.py

--- Raph/performRPCA.py
+++ Raph/performRPCA.py
@@ -1,26 +1,34 @@
 import numpy as np
 import preProcessRPCA as preprocess
 
-""" Perform Robust Principle Component Analysis:
-    -Input = [EEG,params]
-           EEG = EEG Data (must include)
-           lam = Lamda paramter for RPCA (default = 1/(sqrt(# of Colunms))
-           tol = Tolerance (defalut = 1e-7) RPCA param
-           maxIter = Maximum Iterations (deafult = 1000)
-    
-    -Output = [Data, Noise]
-            Data = Corrected Data
-            Noise = Noise removed from the data
-            Original EEG = Data + Noise        
-    
-    -Summary = Performs a Robust Principal Component Analysis on the EEG data with
-            the specified parameters: Lamda, Tolerance, and Maximum number of Iterations.
-            The function outputs the EEG data with the noise removed as well as the nosie
-            that was removed.
-"""
-
-
 def performRPCA(EEG, lam=-1, tol=1e-7, maxIter=1000):
+
+""" Perform Robust Principle Component Analysis:
+
+    Performs a Robust Principal Component Analysis on the EEG data with
+    the specified parameters: Lamda, Tolerance, and Maximum number of Iterations.
+    The function outputs the EEG data with the noise removed as well as the nosie
+    that was removed.
+    
+    Parameters
+    ----------
+    EEG : double numpy array
+        First Parameter, EEG Data (must include)
+    lam : double (default = 1/(sqrt(# of Colunms))
+        Second Parameter, lamda paramter for RPCA (input -1 for default calculation)
+    tol : double (defalut = 1e-7)
+        Third Parameter, tolerance RPCA parameter
+    maxIter : int
+        Fourth Parameter, Maximum Iterations (deafult = 1000)
+    
+    Returns
+    -------
+    Data : double numpy array
+        Corrected Data
+    Noise : double numpy array
+        Noise removed from the data, Original EEG = Data + Noise        
+    
+"""
     #Find lamda if not provided using the Automagic algorithim
     col = EEG.shape;
     if (lam == -1): #if no input lamda, calculate its value
