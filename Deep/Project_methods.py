6cb845346bdd2b77dd619a627a38d99b88f1efd8
deepsoni1996 <54995736+deepsoni1996@users.noreply.github.com>
2019-11-01 11:33:15 -0400

    Deep - week of 10/28/19
    Add makeRatingManually, listSubjects and dirNotHiddens methods of Project class
    Add test codes for methods of Subject class

--- Deep/Project_methods.py
+++ Deep/Project_methods.py
@@ -1,3 +1,5 @@
+import os
+
 class Project:
     def __init__(self):
         # TODO: Need to populate these
@@ -14,8 +16,7 @@ class Project:
         # self.CGV = None
 
     def getRatedCount(self):
-        return len(self.processedList) -\
-        (len(self.notRatedList) + len(self.interpolateList))
+        return len(self.processedList) - (len(self.notRatedList) + len(self.interpolateList))
 
     def toBeInterpolatedCount(self):
         return len(self.interpolateList)
@@ -23,7 +24,60 @@ class Project:
     def areFoldersChanged(self):
         # TODO: Implement isFolderChanged
         dataChanged = self.isFolderChanged(self.dataFolder,
-        self.nSubject, self.nBlock, self.mask, self.params.Settings.trackAllSteps);
-        resultChanged = self.isFolderChanged(self.resultFolder, ...
-        [], self.nProcessedFiles, self.CGV.EXTENSIONS(1).mat, self.params.Settings.trackAllSteps);
-        return dataChanged | | resultChanged;
+        self.nSubject, self.nBlock, self.mask, self.params.Settings.trackAllSteps)
+        resultChanged = self.isFolderChanged(self.resultFolder,
+        [], self.nProcessedFiles, self.CGV.EXTENSIONS(1).mat, self.params.Settings.trackAllSteps)
+        return dataChanged or resultChanged
+
+    @staticmethod
+    def makeRatingManually(block, qRate):
+        """
+        Parameters
+        ----------
+        block - block for which the rate is returned
+        qRate - the rate to be returned
+
+        Returns
+        -------
+        rate - Return qRate if the block is not rated manually. If
+        it is rated manually return 'Manually Rated'.
+        """
+        rate = None
+        if block.isManuallyRated:
+            rate = 'Manually Rated'
+        else:
+            rate = qRate
+        return rate
+    @staticmethod
+    def listSubjects(rootFolder):
+        """
+        Parameters
+        ----------
+        rootFolder - the folder in which subjects are looked for
+
+        Returns
+        -------
+        subjects - list of subjects (dirs) in the folder
+        """
+        subs = os.path.join(rootFolder)
+        subjects = [y for y in os.listdir(subs) if os.path.isdir(y)]
+
+        return subjects
+    @staticmethod
+    def dirNotHiddens(folder):
+        """
+        Parameters
+        ----------
+        folder - The files in this folder are listed
+
+        Returns
+        -------
+        files - the list of files in the folder, excluding the hidden files
+        """
+
+        subs = os.path.join(folder)
+        subjects = [y for y in os.listdir(subs) if os.path.isfile(y)]
+
+        return subjects
+
+
