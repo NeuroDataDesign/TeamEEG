97508dffff85d00a91a08b5fe0d19cc964ca5748
rbechto2 <54995036+rbechto2@users.noreply.github.com>
2019-12-06 01:18:16 -0500

    created preproces class to encapsulate preprocess pipeline of pyautomagic

--- Raph/preformRPCA/rpca.py
+++ Raph/preformRPCA/rpca.py
@@ -5,22 +5,22 @@ from sklearn.decomposition import TruncatedSVD
 def rpca(M,lam,tol,maxIter):
 
     """ Perform Robust Principle Component Analysis:
-    
+
     Performs a Robust Principal Component Analysis on the EEG data with
     the specified parameters: Lamda, Tolerance, and Maximum number of Iterations.
     The function outputs the EEG data with the noise removed as well as the nosie
     that was removed.
-    
+
     Adapted from Cian Scannell - Oct-2017 (https://github.com/cianmscannell/RPCA)
     Computes rpca separation of M into L(low rank) and S(Sparse) using the parameter lam
     this uses the alternating directions augmented method of multipliers.
-    
-    
+
+
     parameters
     ----------
-        M : npumpy.darray 
+        M : npumpy.darray
             1st parameter, EEG Data (must include)
-        lam : double 
+        lam : double
             2nd parameter, Lamda paramter for RPCA (default = 1/(sqrt(# of Colunms))
         tol : double
             3rd parameter, Tolerance (defalut = 1e-7) RPCA param
@@ -28,18 +28,18 @@ def rpca(M,lam,tol,maxIter):
            fourth parameter, Maximum Iterations (deafult = 1000)
     return
     ------
-        Data : npumpy.darray 
+        Data : npumpy.darray
             Corrected Data (Low rank matrix)
-        Error : npumpy.darray 
+        Error : npumpy.darray
             Noise removed from the data (Sparse Matrix)
-            note: M = L + S        
-    
+            note: M = L + S
+
 """
     #Calculate lamda if not provided using the Automagic algorithim
     col = EEG.shape;
     if (lam == -1): #if no input lamda, calculate its value
-        lam = 1 / np.sqrt(col[0]);
-        
+        lam = 1 / np.sqrt(col[1]);
+
     Nr = M.shape[0]
     Nc = M.shape[1]
     norm_2 = np.linalg.norm(M,2)
@@ -53,8 +53,8 @@ def rpca(M,lam,tol,maxIter):
 
 
     L = np.zeros((Nr,Nc))
-    S = np.zeros((Nr,Nc))    
-    
+    S = np.zeros((Nr,Nc))
+
     error = 10
     count = 0
     isRunning = True;
@@ -66,11 +66,11 @@ def rpca(M,lam,tol,maxIter):
         Y = Y + mu*(M-L-S)
         mu = np.minimum(mu*rho,mu_bar)
         error = np.linalg.norm(M-L-S,'fro')/np.linalg.norm(M,'fro')
-        count += 1 
+        count += 1
         if (count >= maxIter):
             isRunning = False;
-            
-            
+
+
     Data = L.reshape(Nr,Nc)
     Error = S.reshape(Nr,Nc)
 
@@ -79,23 +79,23 @@ def rpca(M,lam,tol,maxIter):
 def soft_thres(x,eps):
 
     """
-    Cian Scannell - Oct-2017  
+    Cian Scannell - Oct-2017
     Soft thresholds a matrix x at the eps level
     i.e ST(x,eps)_ij = sgn(x_ij) max(|x_ij| - eps, 0)
-    
+
     parameters
     ----------
         x : npumpy.darray
             first parameter, values to be thersholded
         eps : double
             second parameter, thershold
-            
+
     return
     ------
         np.multiply(a,b) : npumpy.darray
-            thersholded values, where anythign under the threshold 
+            thersholded values, where anythign under the threshold
             was set to zero
-    
+
     """
     a = np.sign(x)
     b = np.maximum((np.fabs(x) - eps), 0)
---
+++ Raph/preproccess/tests/test_performEOGRegression.py
@@ -0,0 +1,9 @@
+import pytest
+import numpy as np
+
+
+def test_performEOGRegression():
+    eeg=np.array([[1, 2, 4, 0.8], [0.1, 0.2, 0.4, 0.9]])
+    eog=np.array([[9, 10, 11, 12], [10, 12, 3, 4]])
+    assert np.array_equal(np.round(performEOGRegression(eeg, eog, {"PerformEOGRegression": "Yes"}), 2), np.round(np.array([[-0.42197603, 0.47275097, 1.71501431, -1.64957357], [-0.07695577, 0.04392939, -0.2369535, 0.23831638]]),2))
+
